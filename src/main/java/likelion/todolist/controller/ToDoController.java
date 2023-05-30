19bb12c174f7f6ee5822ab3ce9b9a6642fa18143
xxo <xxoxec@gmail.com>
2023-05-30 18:47:05 +0900

    feat : 목록 Create 구현

---
+++ src/main/java/likelion/todolist/controller/ToDoController.java
@@ -0,0 +1,37 @@
+package likelion.todolist.controller;
+
+import likelion.todolist.entity.ToDo;
+import likelion.todolist.service.ToDoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ToDoController {
+
+    private final ToDoService toDoService;
+
+    @GetMapping("/todo")
+    public String helloController(Model model) {
+        List<ToDo> toDoList = this.toDoService.findToDos();
+        model.addAttribute("toDoList",toDoList);
+        return "todolist";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/todo";
+    }
+
+    @PostMapping("/todo/create")
+    public String todoCreate(@RequestParam String content){
+        toDoService.saveTodo(content);
+        return "redirect:/todo";
+    }
+}
