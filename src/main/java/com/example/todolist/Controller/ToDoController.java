f9dd546a861ac04d695318b0e6f64c3de69521b5
xxo <xxoxec@gmail.com>
2023-07-22 21:39:10 +0900

    feat : ToDoController 삭제 코드 추가

--- src/main/java/com/example/todolist/Controller/ToDoController.java
+++ src/main/java/com/example/todolist/Controller/ToDoController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,6 +33,12 @@ public class ToDoController {
     @PostMapping("/todo/create")
     public String todoCreate(@RequestParam String content){
         toDoService.saveTodo(content);
+        return "redirect:/todo";
+    }
+    // 삭제 코드
+    @PostMapping("/todo/{todoId}/delete")
+    public String todoDelete(@PathVariable("todoId") Long todoId) {
+        toDoService.deleteTodo(todoId);
         return "redirect:/todo";
     }
 }
