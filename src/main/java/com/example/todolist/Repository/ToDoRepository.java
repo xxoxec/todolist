99c8dc576a357a9e1801d3b1245af0c8f831e8df
xxo <xxoxec@gmail.com>
2023-07-22 21:30:38 +0900

    feat : ToDoRepository 생성

---
+++ src/main/java/com/example/todolist/Repository/ToDoRepository.java
@@ -0,0 +1,37 @@
+package com.example.todolist.Repository;
+
+import com.example.todolist.Entity.ToDo;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ToDoRepository {
+
+    // 엔티티를 관리해주는 엔티티 매니저
+    private final EntityManager em;
+
+    // 데이터를 저장한다.
+    public void save(ToDo todo) {
+        if (todo.getId() == null) {
+            em.persist(todo);
+        } else {
+            em.merge(todo);
+        }
+    }
+
+    // 데이터를 id로 하나만 조회한다.
+    public ToDo findOne(Long id) {
+        return em.find(ToDo.class, id);
+    }
+
+    // 저장된 모든 데이터를 조회한다.
+    public List<ToDo> findAll() {
+        return em.createQuery("select i from ToDo i", ToDo.class)
+                .getResultList();
+    }
+}
+
