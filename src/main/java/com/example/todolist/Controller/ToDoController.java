06db196451e34293292ea49b966e3b147d6d9f22
xxo <xxoxec@gmail.com>
2023-07-22 21:31:05 +0900

    feat : ToDoController 사용자 요청 처리

--- src/main/java/com/example/todolist/Controller/ToDoController.java
+++ src/main/java/com/example/todolist/Controller/ToDoController.java
@@ -1,20 +1,37 @@
 package com.example.todolist.Controller;
 
+import com.example.todolist.Entity.ToDo;
+import com.example.todolist.service.ToDoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ToDoController {
 
+    private final ToDoService toDoService;
+
     @GetMapping("/todo")
-    @ResponseBody
-    public String helloController() {
-        return "hello";
+    public String helloController(Model model) {
+        List<ToDo> toDoList = this.toDoService.findToDos();
+        model.addAttribute("toDoList",toDoList);
+        return "todolist";
     }
 
     @GetMapping("/")
     public String root() {
+        return "redirect:/todo";
+    }
+
+    @PostMapping("/todo/create")
+    public String todoCreate(@RequestParam String content){
+        toDoService.saveTodo(content);
         return "redirect:/todo";
     }
 }
